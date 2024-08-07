63b848219618a2805e3a9e5076f61773a8e45d2c
Haebin Lee <haebin.lee@inbody.com>
2024-08-07 16:22:04 -0700

    add app.yaml for deploy

--- app/todo/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Todo() {
-  return (
-    <>
-      <div className='w-full h-fullflex justify-center items-center'>
-        <h1>This is Todo page</h1>
-      </div>
-    </>
-  );
-}
